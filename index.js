9fc8178f548be3956b592ecc0e6aa5ef2a242c89
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-11-23 23:42:12 +0600

    setup basic server environment

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+/* Middleware */
+app.use(cors())
+app.use(express())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wjboujk.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', (req, res) => {
+    res.send(`Hey i'm calling from car dealer server`)
+})
+app.listen(port, () => {
+    console.log('Server running on ', port)
+})
