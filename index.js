a35537e7eb75b35afff0852078dd3e112fc08fea
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-11-23 23:56:49 +0600

    connect with db and created categories api and get the categories data

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const run = async () => {
+    try {
+        const categoriesCollection = client.db('carDealer').collection('categories')
+
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const result = await categoriesCollection.find(query).toArray()
+            res.send(result)
+        })
+    } finally {
+
+    }
+}
+run().catch(e => console.error(e))
 
 app.get('/', (req, res) => {
     res.send(`Hey i'm calling from car dealer server`)
