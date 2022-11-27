8793f14f2325684d08a964e62abbfd9605214f8d
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-11-27 23:33:51 +0600

    created api seller can delete product

--- index.js
+++ index.js
@@ -149,6 +149,13 @@ const run = async () => {
             res.send(products)
         })
 
+
+        app.delete('/seller-products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await categoryProductsCollection.deleteOne(query)
+            res.send(result)
+        })
         /* Make advertisement seller product */
         app.put('/seller-products/:id', async (req, res) => {
             const id = req.params.id
